484319d4ce2e6dc60ceb6760d76531ea93dbb310
ayasiba <167832748+ayasiba@users.noreply.github.com>
2025-01-25 22:17:22 +0900

    Update ex06-88.js

--- ex06-88.js
+++ ex06-88.js
@@ -1,1 +1,1 @@
-function kimimaro.jpg(){document.getELementById('whokimimaro.jpg').innerHTML='<img src="./image/nptyamu.jpg"alt="オン">';}
+function kimimaro.jpg(){document.getElementById('whokimimaro.jpg').innerHTML='<img src="./image/nptyamu.jpg" alt="オン">';}
