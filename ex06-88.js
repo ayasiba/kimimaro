2e6d08f7f26f6f5a02d3e777dbd67e77b5c29b7e
ayasiba <167832748+ayasiba@users.noreply.github.com>
2025-01-26 01:50:04 +0900

    Update ex06-88.js

--- ex06-88.js
+++ ex06-88.js
@@ -1,1 +1,1 @@
-function gazo6882(){document.getElementById('who6882').innerHTML='<img src="./image/gazo6882.jpg" alt="オン">';}
+function gazo688(){document.getElementById('who688').innerHTML='<img src="./
