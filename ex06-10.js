7931e867fe26920eef6df1317c7134c3dd1a8736
ayasiba <167832748+ayasiba@users.noreply.github.com>
2025-01-25 22:49:44 +0900

    Update ex06-10.js

--- ex06-10.js
+++ ex06-10.js
@@ -1,2 +1,2 @@
 function imgchange1(){document.getElementById('mark').src='./image/nptyamu.jpg';}
-function imgchange2(){document.getElementById('mark').src='./
+function imgchange2(){document.getElementById('mark').src='./image/kimimaro.jpg';}
