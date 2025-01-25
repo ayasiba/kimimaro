daa221cf4687d5f3d6da296a84b97a6e8178a1bb
ayasiba <167832748+ayasiba@users.noreply.github.com>
2025-01-26 05:12:29 +0900

    Update ex07-10.js

--- ex07-10.js
+++ ex07-10.js
@@ -1,5 +1,5 @@
-function add710(){var c=documrnt.getElementById('input7101').value;
-                  var d=documrnt.getElementById('input7102').
+function add710(){var c=document.getElementById('input7101').value;
+                  var d=document.getElementById('input7102').
                   alert(Number(c)/(Number(d)*Number(d)));
                   var bmi=(Number(c)/(Number(d)*Number(d)));
                   if(bmi<18.5){alret('低体重');}
