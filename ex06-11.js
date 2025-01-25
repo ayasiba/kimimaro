abd1a450ef33e91ddd0f4b145c2dfbdc05515ee7
ayasiba <167832748+ayasiba@users.noreply.github.com>
2025-01-25 23:08:49 +0900

    Update ex06-11.js

--- ex06-11.js
+++ ex06-11.js
@@ -1,2 +1,2 @@
-function imgchange3(){document.getElementById('logo').src='./
+function imgchange3(){document.getElementById('logo').src='./image/josaimoji_logo.png';}
 function imgchange4(){document.getElementById('logo').src='./image/josai Logo.jpg';}
