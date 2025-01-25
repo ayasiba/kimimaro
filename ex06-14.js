518d98acd7dbf41a8eb50448a4a599586b183298
ayasiba <167832748+ayasiba@users.noreply.github.com>
2025-01-26 00:27:28 +0900

    Update ex06-14.js

--- ex06-14.js
+++ ex06-14.js
@@ -1,1 +1,2 @@
-function imgchange5(){document.getElementById('chick').src='./
+function imgchange5(){document.getElementById('chick').src='./';}
+function mes2(){document.getElementById('chick').src='./image/opantyu.jpg';}
