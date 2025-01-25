934d78fe040287199d9b8adac20a0b99b8b97d80
ayasiba <167832748+ayasiba@users.noreply.github.com>
2025-01-26 04:59:52 +0900

    Create ex07-10.js

---
+++ ex07-10.js
@@ -0,0 +1,7 @@
+function add710(){var c=documrnt.getElementById('input7101').value;
+                  var d=documrnt.getElementById('input7102').
+                  alert(Number(c)/(Number(d)*Number(d)));
+                  var bmi=(Number(c)/(Number(d)*Number(d)));
+                  if(bmi<18.5){alret('低体重');}
+                  else if(bmi<25){alert('普通');}
+                   else{alert('肥満');}
